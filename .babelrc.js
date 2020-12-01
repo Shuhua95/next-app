ca0ce87f02019f9bef54e442f97f0d5b2944f105
Shuhua95 <o4oban@gmail.com>
2020-12-01 10:03:37 +0800

    fix: Dependabot alerts

--- .babelrc.js
+++ .babelrc.js
@@ -21,8 +21,18 @@ module.exports = {
       "import",
       {
         "libraryName": "antd",
+        "libraryDirectory": "lib",
         "style": "css"
       }
+    ],
+    [
+      "import",
+      {
+        "libraryName": "@ant-design/icons",
+        "libraryDirectory": "lib/icons",
+        "camel2DashComponentName": false
+      },
+      "@ant-design/icons"
     ],
     [
       "styled-components",
